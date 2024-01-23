eecf9bc1fb52026d4863c4044ec5c4921f060e8c
tamburro92 <tamburro92@gmail.com>
2024-01-23 13:30:53 +0100

    steup

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
-# get version from __version__ variable in health_upgrade/__init__.py
+# get version from __version__ variable in venus/__init__.py
 from health_upgrade import __version__ as version
 
 setup(
 	name="health_upgrade",
 	version=version,
-	description="Healthcare customization",
+	description="Health Upgrade customization",
 	author="Tamburro",
 	author_email="t@",
 	packages=find_packages(),
