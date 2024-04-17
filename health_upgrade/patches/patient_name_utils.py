976a32994f9eb9df260c211d1be0593239b1947b
tamburro92 <arturo.ingenito@hotmail.com>
2024-04-17 11:46:26 +0200

    feat: utils

---
+++ health_upgrade/patches/patient_name_utils.py
@@ -0,0 +1,14 @@
+import frappe
+#from erpnext.selling.doctype.customer.customer import get_customer_name
+
+#bench execute health_upgrade.patches.patient_name_utils.rename_patients_name
+def rename_patients_name():
+    doc_type = "Customer"
+    customers = frappe.db.get_all(doc_type, pluck="name")
+    for c in customers:
+        customer = frappe.get_doc(doc_type, c)
+        new_name = customer.get_customer_name()
+        frappe.rename_doc(doc_type,c,new_name)
+
+    frappe.db.commit()
+
